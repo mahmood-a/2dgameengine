94e3a1555f33c985063b635c8c12c87bf3ca6f4f
mahmood <mahmood.abdula@gmail.com>
2026-04-12 23:00:21 +0300

    Updated Lua and Sol header files to latest versions. Added tests for dependencies in the main function.

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 build:
-	g++ -Wall -std=c++17 src/*.cpp -o gameengine -lSDL2
+	g++ -Wall -std=c++17 src/*.cpp -I"./libs" -o gameengine -lSDL2 -lSDL2_image -lSDL2_ttf -lSDL2_mixer -llua5.4
 
 run:
 	./gameengine
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
-#include "SDL2/SDL.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <SDL2/SDL_mixer.h>
+#include <glm/glm.hpp>
+#include "sol/sol.hpp"
 
 using namespace std;
 
 int main() {
-    SDL_Init(SDL_INIT_EVERYTHING);
-    cout << "Hello, world!" << endl;
+    // Try using lua via the sol library
+    sol::state lua;
+    lua.open_libraries(sol::lib::base);
 
+    // Try using GLM library
+    glm::vec2 velocity = glm::vec2(3, 4);
+    velocity = glm::normalize(velocity);
+    
+    // Try to initialize SDL
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    cout << "All dependencies seem to work!" << endl;
     return 0;
 }
